96345ad312aa3746592b48c43dc633b87b2764ea
KampfFlummix <129983918+KampfFlummix@users.noreply.github.com>
2025-04-22 22:10:25 +0200

    Add files via upload

---
+++ Meine dateien/source/server/common/service.h
@@ -0,0 +1,9 @@
+#ifndef __INC_SERVICE_H__
+#define __INC_SERVICE_H__
+#pragma once
+#include "CommonDefines.h"
+#define __HIDE_COSTUME_SYSTEM__ // Kostüm ausblenden
+#define __ACCE_COSTUME_SYSTEM__     // falls du das Accessoire-System auch nutzt
+#define __WEAPON_COSTUME_SYSTEM__   // falls du das Waffenkostüm-System nutzt
+#endif
+//martysama0134's b3d235073c17370fd346b2e61d8c8267
